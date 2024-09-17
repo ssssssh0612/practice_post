7bb102f6771a9db5a232783b689ab02e55b5757a
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-09-17 23:01:30 +0900

    Add: "setter 사용 지양하기 고치는중"

--- src/main/java/com/example/post/practice/post/domain/dto/PostDto.java
+++ src/main/java/com/example/post/practice/post/domain/dto/PostDto.java
@@ -1,8 +1,11 @@
 package com.example.post.practice.post.domain.dto;
 
+import com.example.post.practice.post.domain.entity.Post;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class PostDto {
     private Long id;
     private String memberId;
@@ -10,4 +13,14 @@ public class PostDto {
     private String content;
     private String imageUrl;
     private long likeCount;
+    public static Post toEntity(PostDto postDto) {
+        return Post.builder()
+                .id(postDto.getId())
+                .memberId(postDto.getMemberId())
+                .title(postDto.getTitle())
+                .content(postDto.getContent())
+                .imageUrl(postDto.getImageUrl())
+                .likeCount(postDto.getLikeCount())
+                .build();
+    }
 }
--- src/main/java/com/example/post/practice/post/domain/dto/PostSummaryDto.java
+++ src/main/java/com/example/post/practice/post/domain/dto/PostSummaryDto.java
@@ -1,12 +1,44 @@
 package com.example.post.practice.post.domain.dto;
 
+import com.example.post.practice.post.domain.entity.Post;
+import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
+@Builder
 public class PostSummaryDto {
     private Long id;
     private String title;
     private String content;
     private String imageUrl;
     private Long likeCount;
+
+    public static List<PostSummaryDto> toPostSummaryDtos(List<Post> postList) {
+        List<PostSummaryDto> postSummaryDtoList = new ArrayList<>();
+        for (Post post : postList) {
+            postSummaryDtoList.add(toPostSummaryDto(post));
+        }
+        return postSummaryDtoList;
+    }
+
+    public static PostSummaryDto toPostSummaryDto(Post post) {
+        return PostSummaryDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(sliceContent(post.getContent()))
+                .imageUrl(post.getImageUrl())
+                .likeCount(post.getLikeCount())
+                .build();
+    }
+
+    public static String sliceContent(String content) {
+        if (content.length() > 30) {
+            return content.substring(0, 30) + "...";
+        } else {
+            return content;
+        }
+    }
 }
